cf244c01c9adca20d0b7e10395ad651b272cd79e
Veselin Stoyanov <veselin1996@gmail.com>
2021-11-04 01:53:52 +0200

    Alter covid-stats entrypoint to build external data plots.

--- applications/covid-stats/scripts/entrypoint.sh
+++ applications/covid-stats/scripts/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+echo "$(date '+%d.%m.%Y %H:%M') Start building plots from scraped data..."
 covid_stats
+
+echo "$(date '+%d.%m.%Y %H:%M') Start building plots from external data..."
+covid_stats --external
 
 git add . -A
 git commit -m "Charts automatically generated for $(date '+%d.%m.%Y %H:%M')"
